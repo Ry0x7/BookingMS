d7dd45ffd4505282637159463bc58754b8b2c9ec
Roxy <114163466+Ry0x7@users.noreply.github.com>
2023-11-26 15:22:00 +0800

    Add forms routes for conference, AVR, and little theater

--- app.py
+++ app.py
@@ -15,5 +15,18 @@ def booking():
 def about():
     return render_template('about.html')
 
+@app.route("/forms/conference")
+def conf():
+    return render_template('/forms/conferenceforms.html')
+
+@app.route("/forms/avr")
+def avr():
+    return render_template('/forms/avrforms.html')
+
+@app.route("/forms/littletheater")
+def ltheater():
+    return render_template('/forms/ltforms.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
