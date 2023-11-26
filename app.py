d8fb6e29c1edf96cff520d87ddc1762a73d38ff3
Roxy <114163466+Ry0x7@users.noreply.github.com>
2023-11-26 23:02:24 +0800

    Add booking and customer classes and reservation form handling

--- app.py
+++ app.py
@@ -64,7 +64,91 @@ def login():
             return render_template('loggedin.html')
     return render_template('login.html', error=error)
 
+class Booking:
+    def __init__(self, org_name, startdate, enddate, starttime, endtime, venue, purpose, description):
+        self.booking_id = str(uuid.uuid4())
+        self.org_name = org_name
+        self.startdate = startdate
+        self.enddate = enddate
+        self.starttime = starttime
+        self.endtime = endtime
+        self.venue = venue
+        self.purpose = purpose
+        self.description = description
 
+    def to_dict(self):
+        return {
+            '_id': ObjectId(),
+            'booking_id': self.booking_id,
+            'org_name': self.org_name,
+            'startdate': self.startdate,
+            'enddate': self.enddate,
+            'starttime': self.starttime,
+            'endtime': self.endtime,
+            'venue': self.venue,
+            'purpose': self.purpose,
+            'description': self.description
+        }
+
+class Customer:
+    def __init__(self, email, contact, address, org_type, company):
+        self.cid = str(uuid.uuid4())
+        self.email = email
+        self.contact = contact
+        self.org_type = org_type
+        self.company = company
+
+    def to_dict(self):
+        return {
+            '_id': ObjectId(),
+            'cid': self.cid,
+            'email': self.email,
+            'contact': self.contact,
+            'address': self.address,
+            'org_type': self.org_type,
+            'company': self.company
+        }
+
+@app.route("/forms", methods=['POST'])
+def reservation_post():
+    try:
+        reserv_collection = db['reservation']
+
+        startdate = request.form.get('start-date')
+        enddate = request.form.get('end-date')
+
+        existing_booking = reserv_collection.find_one({
+            'startdate': {'$lte': enddate},
+            'enddate': {'$gte': startdate}
+        })
+
+        if existing_booking:
+            return "Error: There is already a booking in the specified date range."
+
+        org_name = request.form.get('org_name')
+        starttime = request.form.get('start-time')
+        endtime = request.form.get('end-time')
+        venue = request.form.get('room_number')
+        purpose = request.form.get('purpose')
+        description = request.form.get('description')
+
+        booking = Booking(org_name, startdate, enddate, starttime, endtime, venue, purpose, description)
+        reserv_collection.insert_one(booking.to_dict())
+
+        cust_collection = db['customer']
+        
+        email = request.form.get('org_email')
+        contact = request.form.get('org_phone')
+        address = request.form.get('org_address')
+        org_type = request.form.get('org_type') 
+        company = request.form.get('org_company')
+
+        customer = Customer(email, contact, address, org_type, company)
+        cust_collection.insert_one(customer.to_dict())
+
+        return redirect(url_for('booking'))
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
 if __name__ == "__main__":
     app.run(debug=True)
